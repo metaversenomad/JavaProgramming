6e37af222dd2909bd2e889b07affc2755a87c049
Mr.OGUZ <96841213+metaversenomad@users.noreply.github.com>
2023-11-22 22:03:44 +0100

    Add files via upload

--- java/practice/NestedLoop.java
+++ java/practice/NestedLoop.java
@@ -3,8 +3,11 @@ package practice;
 public class NestedLoop {
     public static void main(String[] args) {
 
-        for (int i=1; i<=5; i++){
-            System.out.println("i = " + i);
+        for (int j = 1; j <= 5; j++) {
+            for (int i = 1; i <= 5; i++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(" ");
         }
     }
 }

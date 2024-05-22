f0291b4e54564f332e870b4e62f5ce68c131bfd2
Mr.OGUZ <96841213+metaversenomad@users.noreply.github.com>
2024-05-22 21:02:50 +0200

    Add files via upload
    v1

---
+++ JavaProgramming/src/practice/IfElseStatement.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+
+        int score = 55;
+
+        if (score >= 60) {
+            System.out.println("Passed");
+        }else {
+            System.out.println("Failed");
+        }
+    }
+}
---
+++ JavaProgramming/src/practice_day02/scnrprcts4.java
@@ -0,0 +1,8 @@
+package practice_day02;
+
+public class scnrprcts4 {
+    public static void main(String[] args) {
+        System.out.println("hellos");
+
+    }
+}

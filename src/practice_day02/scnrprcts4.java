c02cb749c152e7772b4fd7df17d9af94e38f1f72
metaversenomad <mr.oguz01@gmail.com>
2024-05-22 21:34:28 +0200

    v2

--- src/practice_day02/scnrprcts4.java
+++ src/practice_day02/scnrprcts4.java
@@ -2,7 +2,7 @@ package practice_day02;
 
 public class scnrprcts4 {
     public static void main(String[] args) {
-        System.out.println("hellos");
+        System.out.println("hello world");
 
     }
 }
